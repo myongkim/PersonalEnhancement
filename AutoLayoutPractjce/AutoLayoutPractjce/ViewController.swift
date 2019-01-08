f42e3a556b54187501bcf60dab724d845bda3bf9
Isaac Kim <myong328@gmail.com>
2019-01-08 16:40:45 +0900

    auto Layout practice with pickerView practice

---
+++ AutoLayoutPractjce/AutoLayoutPractjce/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  AutoLayoutPractjce
+//
+//  Created by Isaac Kim on 08/01/2019.
+//  Copyright © 2019 Isaac Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var mainView: UIView!
+    @IBOutlet weak var firstViewHeightProportion: NSLayoutConstraint!
+    @IBOutlet weak var fourthView: UIView!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    let months: [String] = ["January","Febuary","March","April","May","June","July","August","September","October","November","December"]
+  
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+        
+        
+        
+        
+    }
+
+    func setup() {
+        var firstViewHeightValue = firstViewHeightProportion.constant
+        firstViewHeightValue = (mainView.frame.height * 0.2) - 5
+        
+        print("firstViewheight:", firstViewHeightValue)
+        print("mainvieHeight:", mainView.frame.height)
+        
+        // setup FourthView Label
+        let monthLabel = UILabel(frame: CGRect(x: 0, y: 0, width: fourthView.frame.width, height: fourthView.frame.height))
+        monthLabel.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        monthLabel.textAlignment = .center
+        fourthView.addSubview(monthLabel)
+        
+        
+    }
+
+}
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return months.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return months[row]
+    }
+    
+    
+}
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        let rowHeight: CGFloat = 50
+        return rowHeight
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("row:", months[row])
+  
+        let monthLabel = UILabel(frame: CGRect(x: 0, y: 0, width: fourthView.frame.width, height: fourthView.frame.height))
+        monthLabel.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        monthLabel.textAlignment = .center
+        fourthView.addSubview(monthLabel)
+        monthLabel.text = months[row]
+        
+    }
+    
+}
+
