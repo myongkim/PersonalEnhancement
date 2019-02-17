0ac5847a03222856dcffb243f9ffca2026aeda71
Isaac Kim <myong328@gmail.com>
2019-02-17 10:24:46 -0800

    for loop and while loop practice with questions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -277,3 +277,196 @@ func lunarYearFinder(year: Int) {
 }
 
 lunarYearFinder(year: 4)
+
+
+
+//////////////////////
+
+// For - In Loop
+print("\n===============[ for ]========================\n")
+
+
+/***************************************************
+ for item in <#items#> {
+ <#code#>
+ }
+ ***************************************************/
+
+for index in 2...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+// Wildcard pattern
+
+for _ in 0...3 {
+    print("hello")
+}
+
+for chr in "Hello" {
+    print(chr, terminator: " ")
+}
+print()
+
+let list = ["Swift", "programming", "Language"]
+for str in list {
+    print(str)
+}
+
+//for 문을 이용하여 3 의 10 제곱에 대한 결과값을 표현하는 코드 구현
+let base = 3
+let power = 10
+var answer = 1
+
+for _ in 1...power {
+    answer *= base
+}
+print(answer)
+
+
+
+//for 문을 이용하여 10부터 1까지 내림차순으로 출력하는 코드 구현
+
+
+
+for num in (1...10).reversed() {
+    print(num, terminator: " ")
+}
+
+
+// while Loops
+
+print("\n===============[ while ]========================\n")
+
+/***************************************************
+ while <#condition#> {
+ <#code#>
+ }
+ 
+ - 루프를 통과하는 각 패스의 시작 부분에서 조건을 평가
+ ***************************************************/
+
+
+// add 1 to 100
+var num = 0
+var sum = 0
+
+while num <= 100 {
+    sum += num
+    num += 1
+}
+
+print(sum)
+
+
+/***************************************************
+ repeat {
+ <#code#>
+ } while <#condition#>
+ 
+ - 루프를 통과하는 각 패스의 끝 부분에서 조건을 평가
+ - 다른 언어에서는 do - while 문으로 많이 사용
+ ***************************************************/
+
+
+num = 0
+sum = 0
+
+repeat {
+    sum += num
+    num += 1
+} while num <= 100
+
+print(sum)
+
+
+//1 ~ 9 사이의 숫자를 입력받아 해당 숫자에 해당하는 구구단의 내용을 출력하는 함수
+
+func multiplicationToNine(number: Int) {
+    
+    
+    for num in 1...9 {
+        print(num * number, terminator: " ")
+        
+    }
+    
+}
+multiplicationToNine(number: 8)
+
+
+//정수 하나를 입력받아 그 수의 Factorial 을 구하는 함수
+
+func factorial(number: Int) {
+    var answer = 1
+    
+    for num in 1...number {
+        answer *= num
+        print(answer)
+    }
+    
+}
+factorial(number: 5)
+
+
+//정수 두개를 입력받아 첫 번째 수를 두 번째 수의 횟수만큼 곱한 값을 반환하는 함수
+
+func calculationForFirstNumber(firstNumber: Int, SecondNumber: Int) {
+    
+    var answer = firstNumber
+    
+    for i in 1...SecondNumber {
+        answer *= i
+        
+    }
+    print(answer)
+}
+calculationForFirstNumber(firstNumber: 4, SecondNumber: 5)
+
+
+//정수 하나를 입력받아 각 자리수 숫자들의 합을 반환해주는 함수
+
+func addThemUp(number: Int) {
+    
+    var answer = 0
+    
+    for i in 1...number {
+    
+        answer += i
+        
+    }
+    print(answer)
+}
+
+addThemUp(number: 5)
+
+
+//100 이하의 자연수 중 3과 5의 공배수를 모두 출력하는 함수
+
+
+func fuzzBizz() {
+ 
+    for i in 1...100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print(i, terminator: " ")
+        } else if i % 3 == 0 {
+            print(i, terminator: " ")
+        } else if i % 5 == 0 {
+            print(i, terminator: " ")
+        }
+       
+        
+    }
+    
+}
+
+fuzzBizz()
+
+
+
+
+
+
+
+
+
+
+
