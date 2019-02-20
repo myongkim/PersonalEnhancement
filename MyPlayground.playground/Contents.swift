d62a97aa64f13fd7a3af9c777d684c50ffbe5c9c
Isaac Kim <myong328@gmail.com>
2019-02-19 17:42:14 -0800

    modified answer

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -516,3 +516,46 @@ let number: Int = 10
 
 
 
+
+func transformer(_ array: [Int]) -> [String] {
+        
+        var spellArray: Array<String> = []
+        
+        for element in array {
+            let numberForTimes = element
+            let spellFormatNumber = NumberFormatter.localizedString(from: NSNumber(value: element), number: .spellOut)
+            
+            for _ in 0..<numberForTimes {
+                spellArray.append(spellFormatNumber)
+            }
+        }
+        
+        return spellArray
+}
+
+
+transformer([0,3,2,5])
+
+func transformArray(array: [Int]) -> [String] {
+    
+    var modifiedArray: [String] = []
+    
+    for element in array {
+        let numberOfTimes = element
+        let spellFormatNumber = NumberFormatter.localizedString(from: NSNumber(value: element), number: .spellOut)
+        
+        for _ in 0..<numberOfTimes {
+            modifiedArray.append(spellFormatNumber)
+        }
+    }
+    return modifiedArray
+    
+}
+
+transformArray(array: [3,0,2,1,5])
+
+
+
+
+
+
